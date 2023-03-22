0650828c3b2d61653a58a3fed97d433b2952d91d
youcefdoukhi <youcef.doukhi.dz@gmail.com>
2023-03-22 16:18:18 +0100

    16:18 22/03/2023

--- lib/data.dart
+++ lib/data.dart
@@ -1,37 +1,386 @@
 class DownloadItems {
+  static DownloadItem getDownloadItem(int numBook, int numCourse) {
+    return book01[numCourse];
+  }
+
+  static const link = "http://10.1.1.19/el_madradja/";
   static const book01 = [
     DownloadItem(
-      name: '01_01',
-      url: 'http://10.1.1.19/el_madradja/01/01.mp3',
+      name: 'الدرس 01',
+      url: '${link}01/01.mp3',
     ),
     DownloadItem(
-      name: '01_02',
-      url: 'http://10.1.1.19/el_madradja/01/02.mp3',
+      name: 'الدرس 02',
+      url: '${link}01/02.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 03',
+      url: '${link}01/03.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 04',
+      url: '${link}01/04.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 05',
+      url: '${link}01/05.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 06',
+      url: '${link}01/06.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 07',
+      url: '${link}01/07.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 08',
+      url: '${link}01/08.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 09',
+      url: '${link}01/09.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 10',
+      url: '${link}01/10.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 11',
+      url: '${link}01/11.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 12',
+      url: '${link}01/12.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 13',
+      url: '${link}01/13.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 14',
+      url: '${link}01/14.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 15',
+      url: '${link}01/15.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 16',
+      url: '${link}01/16.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 17',
+      url: '${link}01/17.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 18',
+      url: '${link}01/18.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 19',
+      url: '${link}01/19.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 20',
+      url: '${link}01/20.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 21',
+      url: '${link}01/21.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 22',
+      url: '${link}01/22.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 23',
+      url: '${link}01/23.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 24',
+      url: '${link}01/24.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 25',
+      url: '${link}01/25.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 26',
+      url: '${link}01/26.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 27',
+      url: '${link}01/27.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 28',
+      url: '${link}01/28.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 29',
+      url: '${link}01/29.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 30',
+      url: '${link}01/30.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 31',
+      url: '${link}01/31.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 32',
+      url: '${link}01/32.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 33',
+      url: '${link}01/33.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 34',
+      url: '${link}01/34.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 35',
+      url: '${link}01/35.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 36',
+      url: '${link}01/36.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 37',
+      url: '${link}01/37.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 38',
+      url: '${link}01/38.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 39',
+      url: '${link}01/39.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 40',
+      url: '${link}01/40.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 41',
+      url: '${link}01/41.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 42',
+      url: '${link}01/42.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 43',
+      url: '${link}01/43.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 44',
+      url: '${link}01/44.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 45',
+      url: '${link}01/45.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 46',
+      url: '${link}01/46.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 47',
+      url: '${link}01/47.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 48',
+      url: '${link}01/48.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 49',
+      url: '${link}01/49.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 50',
+      url: '${link}01/50.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 51',
+      url: '${link}01/51.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 52',
+      url: '${link}01/52.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 53',
+      url: '${link}01/53.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 54',
+      url: '${link}01/54.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 55',
+      url: '${link}01/55.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 56',
+      url: '${link}01/56.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 57',
+      url: '${link}01/57.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 58',
+      url: '${link}01/58.mp3',
     ),
   ];
 
   static const book02 = [
     DownloadItem(
-      name: '02_01',
-      url: 'http://10.1.1.19/el_madradja/02/01.mp3',
+      name: 'الدرس 01',
+      url: '${link}02/01.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 02',
+      url: '${link}02/02.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 03',
+      url: '${link}02/03.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 04',
+      url: '${link}02/04.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 05',
+      url: '${link}02/05.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 06',
+      url: '${link}02/06.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 07',
+      url: '${link}02/07.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 08',
+      url: '${link}02/08.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 09',
+      url: '${link}02/09.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 10',
+      url: '${link}02/10.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 11',
+      url: '${link}02/11.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 12',
+      url: '${link}02/12.mp3',
     ),
   ];
   static const book03 = [
     DownloadItem(
-      name: '03_01',
-      url: 'http://10.1.1.19/el_madradja/03/01.mp3',
+      name: 'الدرس 01',
+      url: '${link}03/01.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 02',
+      url: '${link}03/02.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 03',
+      url: '${link}03/03.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 04',
+      url: '${link}03/04.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 05',
+      url: '${link}03/05.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 06',
+      url: '${link}03/06.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 07',
+      url: '${link}03/07.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 08',
+      url: '${link}03/08.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 09',
+      url: '${link}03/09.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 10',
+      url: '${link}03/10.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 11',
+      url: '${link}03/11.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 12',
+      url: '${link}03/12.mp3',
     ),
   ];
   static const book04 = [
     DownloadItem(
-      name: '04_01',
-      url: 'http://10.1.1.19/el_madradja/04/01.mp3',
+      name: 'الدرس 01',
+      url: '${link}04/01.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 02',
+      url: '${link}04/02.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 03',
+      url: '${link}04/03.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 04',
+      url: '${link}04/04.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 05',
+      url: '${link}04/05.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 06',
+      url: '${link}04/06.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 07',
+      url: '${link}04/07.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 08',
+      url: '${link}04/08.mp3',
+    ),
+    DownloadItem(
+      name: 'الدرس 09',
+      url: '${link}04/09.mp3',
     ),
   ];
   static const book05 = [
     DownloadItem(
       name: '05_01',
-      url: 'http://10.1.1.19/el_madradja/05/01.mp3',
+      url: '${link}05/01.mp3',
     ),
   ];
 }
